9217dff265573d769aac45aa6d9b844b81dc3579
Dennis Svedberg <densvedberg@gmail.com>
2021-01-29 03:26:13 +0100

    v3d logic test

--- ph/script.js
+++ ph/script.js
@@ -14,12 +14,14 @@ class Main {
 
     // Graph
     this.plot = document.getElementById('plot');
-    this.config = {responsive: true, yaxis: {range: [1, 7], title: {text: 'pH', font: {size: 20}}}, xaxis: {range: [0, 2000], title: {text: 'μl HCl', font: {size: 20}}},
-      margin: {t: 30, b: 50, l: 50, r: 30}};
-
-    this.data = [{name: 'pH', line: {shape: 'spline'}, type: 'line', x: this.x, y: this.y},
-      {type: 'line', name: 'pKa', x: [this.pka, this.pka], y: [0, 8]}];
-    Plotly.newPlot(this.plot, this.data, this.config );
+    this.layout = {responsive: true, margin: {t: 30, b: 50, l: 50, r: 30},
+      yaxis: {range: [1, 7], title: {text: 'pH', font: {size: 20}}, dtick: 0.5},
+      xaxis: {range: [0, 2000], title: {text: 'μl HCl added', font: {size: 20}}, dtick: 100},
+      };
+    this.config = {displayModeBar: false}
+    this.data = [{name: 'pH', color: 'rgb(42, 71, 101)', line: {shape: 'spline'}, type: 'line', x: this.x, y: this.y, mode: 'lines+markers'},
+      {type: 'line', name: 'Ekvivalenspunkt', hoverinfo: 'skip', x: [this.pka, this.pka], y: [0, 8]}];
+    Plotly.newPlot(this.plot, this.data, this.layout, this.config);
   }
 
   step() {
@@ -69,7 +71,7 @@ class Main {
     Plotly.plot(
       'plot',
       this.data,
-      this.config)
+      this.layout)
     .then(
       function(gd) {
         Plotly.toImage(gd, {
@@ -115,7 +117,8 @@ var element = document.getElementById("downloadPDF");
 element.addEventListener("click", simulation.save_pdf);
 
 var slider = document.getElementById("pkarange");
-
+var eqvalue = document.getElementById("eqvp");
 slider.oninput = function() {
+  eqvp.innerHTML = this.value;
   simulation.slide(this.value);
 }
